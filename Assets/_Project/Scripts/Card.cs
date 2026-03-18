a2c0de9c39806cfa81a9e4589f053e3b499ddb30
ChaitanyaShah1305 <chjetfire09@gmail.com>
2026-03-18 14:46:18 +0530

    add main card match logic

--- Assets/_Project/Scripts/Card.cs
+++ Assets/_Project/Scripts/Card.cs
@@ -2,59 +2,84 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Card : MonoBehaviour
+namespace _Project.Scripts
 {
-    public int cardID;
-    public Image cardImage;
-    public Sprite backSprite;
-    public Sprite frontSprite;
-    [HideInInspector] public bool isFaceUp = false;
-    [HideInInspector] public bool isMatched = false;
-    private const float FlipDuration = 0.15f; // Total time for flip
-
-    public void Init(int id, Sprite front)
+    public class Card : MonoBehaviour
     {
-        cardID = id;
-        frontSprite = front;
-        cardImage.sprite = backSprite;
-        isMatched = false;
-        isFaceUp = false;
-        gameObject.SetActive(true);
-    }
+        public int cardID;
+        public Image cardImage;
+        public Sprite backSprite;
+        public Sprite frontSprite;
+        [HideInInspector] public bool isFaceUp = false;
+        [HideInInspector] public bool isMatched = false;
+        private const float FlipDuration = 0.15f, ScaleDownDuration = 0.15f;
 
-    public void OnCardClick()
-    {
-        // Ignore if already face up, matched, or if the manager is full
-        if (isFaceUp || isMatched) return;
-        StartCoroutine((Flip(true)));
-    }
-
-    private IEnumerator Flip(bool faceUp)
-    {
-        isFaceUp = faceUp;
-        var time = 0f;
-
-        var startRotation = transform.rotation;
-        var endRotation = transform.rotation * Quaternion.Euler(0, 180, 0);
-
-        var spriteSwapped = false;
-
-        while (time < FlipDuration)
+        public void Init(int id, Sprite front)
         {
-            time += Time.deltaTime;
-            var progress = time / FlipDuration;
+            cardID = id;
+            frontSprite = front;
+            cardImage.sprite = backSprite;
+            isMatched = false;
+            isFaceUp = false;
+            gameObject.SetActive(true);
+        }
 
-            // Rotate the card
-            transform.rotation = Quaternion.Slerp(startRotation, endRotation, progress);
+        public void OnCardClick()
+        {
+            // Ignore if already face up, matched, or if the manager is full
+            if (isFaceUp || isMatched) return;
+            GameManager.Instance.OnCardFlipped(this);
+        }
 
-            // Swap sprite at the midpoint (90 degrees)
-            if (!spriteSwapped && progress >= 0.5f)
+        public IEnumerator Flip(bool faceUp)
+        {
+            isFaceUp = faceUp;
+            var time = 0f;
+
+            var startRotation = transform.rotation;
+            var endRotation = transform.rotation * Quaternion.Euler(0, 180, 0);
+
+            var spriteSwapped = false;
+
+            while (time < FlipDuration)
             {
-                cardImage.sprite = faceUp ? frontSprite : backSprite;
-                spriteSwapped = true;
-            }
+                time += Time.deltaTime;
+                var progress = time / FlipDuration;
 
-            yield return null;
+                // Rotate the card
+                transform.rotation = Quaternion.Slerp(startRotation, endRotation, progress);
+
+                // Swap sprite at the midpoint (90 degrees)
+                if (!spriteSwapped && progress >= 0.5f)
+                {
+                    cardImage.sprite = faceUp ? frontSprite : backSprite;
+                    spriteSwapped = true;
+                }
+
+                yield return null;
+            }
+        }
+
+        public void SetMatched()
+        {
+            isMatched = true;
+            // Remove Card
+            StartCoroutine(RemoveCard());
+        }
+
+        private IEnumerator RemoveCard()
+        {
+            yield return new WaitForSeconds(0.3f);
+            var startScale = transform.localScale;
+            var endScale = Vector3.zero;
+            var time = 0f;
+            while (time < ScaleDownDuration)
+            {
+                time += Time.deltaTime;
+                // Scale down the card
+                transform.localScale = Vector3.Lerp(startScale, endScale, time / ScaleDownDuration);
+                yield return null;
+            }
         }
     }
 }
--- Assets/_Project/Scripts/GameManager.cs
+++ Assets/_Project/Scripts/GameManager.cs
@@ -1,74 +1,115 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
-public class GameManager : MonoBehaviour
+namespace _Project.Scripts
 {
-    public static GameManager Instance;
-
-    [Header("UI & Grid References")]
-    public GameObject cardPrefab;
-    public Transform gridParent;
-    public GridLayoutGroup gridLayout;
-    public Sprite[] cardSprites; // Assign your card face textures here
-
-    private List<Card> _allCards = new List<Card>();
-    private int _currentRows, _currentCols;
-
-    private void Awake()
+    public class GameManager : MonoBehaviour
     {
-        if (Instance == null) Instance = this;
-    }
+        public static GameManager Instance;
 
-    private void Start()
-    {
-        _currentRows = 2;
-        _currentCols = 2;
-        SetupGrid(_currentRows, _currentCols);
-        var deck = GenerateDeck(_currentRows, _currentCols);
-        foreach (var id in deck)
+        [Header("UI & Grid References")]
+        public GameObject cardPrefab;
+        public Transform gridParent;
+        public GridLayoutGroup gridLayout;
+        public Sprite[] cardSprites; // Assign your card face textures here
+
+        private List<Card> _allCards;
+        private int _currentRows, _currentCols;
+        private Card _activeCard;
+
+        private void Awake()
         {
-            CreateCard(id);
+            if (Instance == null) Instance = this;
         }
-    }
 
-    // --- Grid & Deck Management ---
-    private void SetupGrid(int rows, int cols)
-    {
-        var rect = gridParent.GetComponent<RectTransform>();
-        var availableW = rect.rect.width - (gridLayout.padding.left + gridLayout.padding.right);
-        var availableH = rect.rect.height - (gridLayout.padding.top + gridLayout.padding.bottom);
-
-        var cellW = (availableW - (gridLayout.spacing.x * (cols - 1))) / cols;
-        var cellH = (availableH - (gridLayout.spacing.y * (rows - 1))) / rows;
-
-        var size = Mathf.Min(cellW, cellH);
-        gridLayout.cellSize = new Vector2(size, size);
-        gridLayout.constraintCount = cols;
-    }
-
-    // Shuffle and generate deck
-    private List<int> GenerateDeck(int r, int c)
-    {
-        var deck = new List<int>();
-        for (var i = 0; i < (r * c) / 2; i++) { deck.Add(i); deck.Add(i); }
-
-        // Fisher-Yates Shuffle
-        for (var i = deck.Count - 1; i > 0; i--)
+        private void Start()
         {
-            var rnd = Random.Range(0, i + 1);
-            (deck[i], deck[rnd]) = (deck[rnd], deck[i]);
+            _currentRows = 2;
+            _currentCols = 2;
+            _allCards = new List<Card>();
+            SetupGrid(_currentRows, _currentCols);
+            var deck = GenerateDeck(_currentRows, _currentCols);
+            foreach (var id in deck)
+            {
+                CreateCard(id);
+            }
         }
-        return deck;
-    }
 
-    // Create cards
-    private void CreateCard(int id)
-    {
-        var go = Instantiate(cardPrefab, gridParent);
-        var c = go.GetComponent<Card>();
-        c.Init(id, cardSprites[id]);
-        _allCards.Add(c);
+        // --- Grid & Deck Management ---
+        private void SetupGrid(int rows, int cols)
+        {
+            var rect = gridParent.GetComponent<RectTransform>();
+            var availableW = rect.rect.width - (gridLayout.padding.left + gridLayout.padding.right);
+            var availableH = rect.rect.height - (gridLayout.padding.top + gridLayout.padding.bottom);
+
+            var cellW = (availableW - (gridLayout.spacing.x * (cols - 1))) / cols;
+            var cellH = (availableH - (gridLayout.spacing.y * (rows - 1))) / rows;
+
+            var size = Mathf.Min(cellW, cellH);
+            gridLayout.cellSize = new Vector2(size, size);
+            gridLayout.constraintCount = cols;
+        }
+
+        // Shuffle and generate deck
+        private List<int> GenerateDeck(int r, int c)
+        {
+            var deck = new List<int>();
+            for (var i = 0; i < (r * c) / 2; i++) { deck.Add(i); deck.Add(i); }
+
+            // Fisher-Yates Shuffle
+            for (var i = deck.Count - 1; i > 0; i--)
+            {
+                var rnd = Random.Range(0, i + 1);
+                (deck[i], deck[rnd]) = (deck[rnd], deck[i]);
+            }
+            return deck;
+        }
+
+        // Create cards
+        private void CreateCard(int id)
+        {
+            var go = Instantiate(cardPrefab, gridParent);
+            var c = go.GetComponent<Card>();
+            c.Init(id, cardSprites[id]);
+            _allCards.Add(c);
+        }
+
+        public void OnCardFlipped(Card card)
+        {
+            StartCoroutine(card.Flip(true));
+
+            // If no card is opened, make this as an active card
+            if (_activeCard == null)
+            {
+                _activeCard = card;
+                return; 
+            }
+
+            // Check for the match
+            var c1 = _activeCard;
+            var c2 = card;
+            _activeCard = null;
+            StartCoroutine(ProcessMatch(c1, c2));
+        }
+
+        private IEnumerator ProcessMatch(Card c1, Card c2)
+        {
+            yield return new WaitForSeconds(0.4f);
+            if (c1.cardID == c2.cardID)
+            {
+                // Match Found
+                c1.SetMatched();
+                c2.SetMatched();
+            }
+            else
+            {
+                // Match Failed
+                StartCoroutine(c1.Flip(false));
+                StartCoroutine(c2.Flip(false));
+            }
+        }
     }
 }
